b636bcad33de52fb8e63b590a1307bb91801c732
Bruno Righi <brunorighi267@gmail.com>
2023-03-06 08:35:28 -0300

    classe DAO

--- DAO/DAO.php
+++ DAO/DAO.php
@@ -1,14 +1,33 @@
 <?php
 namespace App\DAO;
-use \PDO;
 
-abstract class DAO
+use Exception;
+use PDO;
+use PDOException;
+
+abstract class DAO extends PDO
 {
     protected $conexao;
 
     public function __construct()
-    {        
-        $dsn = "mysql:host=localhost:3307;dbname=db_apicep";        
-        $this->conexao = new PDO($dsn, 'root', 'etecjau');
+    {
+        try
+        {
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            ];
+
+        $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname="
+        . $_ENV['db']['database'];
+
+        $this->conexao = new PDO($dsn, $_ENV['db']['user'],
+        $_ENV['db']['pass'], $options);
+    
+    }
+        catch (PDOException $e)
+        {
+            throw new Exception("Ocorreu um erro ao tentar conectar ao MySQL", 0, $e);
+        }
     }
 }
