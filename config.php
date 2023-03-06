c42bfba02b88680adca3fef9bbbf3125ca65479e
Bruno Righi <brunorighi267@gmail.com>
2023-03-06 08:38:49 -0300

    arquivo config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEDIR', dirname(__FILE__, 2));
+
+//Banco de dados
+$_ENV['db']['host'] = 'localhost:3307';
+$_ENV['db']['user'] = 'root';
+$_ENV['db']['pass'] = 'etecjau';
+$_ENV['db']['database'] = 'apicep';
